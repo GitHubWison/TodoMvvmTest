6ea2d3695f76d74adae23b2fabee32deaa0a4ff1
GitHubWison <15995448871@163.com>
2017-03-13 17:00:11 +0800

    控件测试

---
+++ app/src/main/java/xu/qiwei/com/todomvvmtest/twobindingtest/TwoBindActivity.java
@@ -0,0 +1,15 @@
+package xu.qiwei.com.todomvvmtest.twobindingtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xu.qiwei.com.todomvvmtest.R;
+
+public class TwoBindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_bind);
+    }
+}
